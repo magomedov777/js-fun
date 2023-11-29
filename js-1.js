62e44ba5cd735b28c0ce3218eb1bf9cb997f65ff
Arkadiy <alibullterrier@gmail.com>
2023-11-29 05:48:34 +0200

    for cycle

--- js-1.js
+++ js-1.js
@@ -3,3 +3,11 @@ const newArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 for (let i = 0; i < newArray.length; i++) {
   console.log(i)
 }
+
+const newArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+for (let i = 0; i < newArray.length; i++) {
+  let arr = newArr.reverse()
+  console.log(arr)
+  break
+}
