8f80381d632f6f386ec97fa802f378d358234415
Arkadiy <alibullterrier@gmail.com>
2023-11-29 05:50:27 +0200

    for practic for remove item

--- js-1.js
+++ js-1.js
@@ -67,3 +67,11 @@ function reverseSeq(n) {
   }
   return res
 }
+
+function removeEveryOther(arr) {
+  let newArray = []
+  for (let i = 0; i < arr.length; i += 2) {
+    newArray.push(arr[i])
+  }
+  return newArray
+}
