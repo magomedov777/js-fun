fe9e5428dd8ba39994409f4558a263e1246b58e2
Arkadiy <alibullterrier@gmail.com>
2023-12-01 05:28:46 +0200

    even or odd task

--- js-2.js
+++ js-2.js
@@ -1,3 +1,12 @@
 evenOrOdd = (number) => (number % 2 === 0 ? "Even" : "Odd")
 
 //Even or Odd | short solution
+
+function evenOrOdd(number) {
+  if (number % 2 === 0) {
+    return "Even"
+  }
+  return "Odd"
+}
+
+//other solution
