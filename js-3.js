93f79aa24894ab3b0aa1fef2753ff717bc686516
Arkadiy <alibullterrier@gmail.com>
2023-12-02 00:01:21 +0200

    squareSum solution, short syntax

--- js-3.js
+++ js-3.js
@@ -2,3 +2,6 @@
 function squareSum(numbers) {
   return numbers.map((el) => el ** 2).reduce((acc, val) => acc + val, 0)
 }
+
+//short syntax | arrow
+squareSum = (num) => num.map((el) => el ** 2).reduce((acc, val) => acc + val, 0)
