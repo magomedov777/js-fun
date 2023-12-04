9e36184168cad0f7c93a8d6032c4bf950a57b045
Arkadiy <alibullterrier@gmail.com>
2023-12-04 05:53:05 +0200

    basic mathematical operation

---
+++ js-4.js
@@ -0,0 +1,18 @@
+//Basic Mathematical Operations
+
+//if
+
+function basicOp(operation, value1, value2) {
+  if (operation === "+") {
+    return value1 + value2
+  }
+  if (operation === "-") {
+    return value1 - value2
+  }
+  if (operation === "*") {
+    return value1 * value2
+  }
+  if (operation === "/") {
+    return value1 / value2
+  }
+}
