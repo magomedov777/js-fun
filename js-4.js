d90a6fb6b1e48c4a7698ad6037bbe01a3a731428
Arkadiy <alibullterrier@gmail.com>
2023-12-04 05:54:04 +0200

    basic mathematical operation, short solution

--- js-4.js
+++ js-4.js
@@ -33,3 +33,11 @@ function basicOp(operation, value1, value2) {
       return 0
   }
 }
+
+//shorts solution
+
+function basicOp(o, a, b) {
+  return eval(a + o + b)
+}
+
+//-----------------------------------
