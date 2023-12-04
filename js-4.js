af35dbdb2e22c774283b177f93f4ff49348b0f3e
Arkadiy <alibullterrier@gmail.com>
2023-12-04 05:53:23 +0200

    basic mathematical operation

--- js-4.js
+++ js-4.js
@@ -16,3 +16,20 @@ function basicOp(operation, value1, value2) {
     return value1 / value2
   }
 }
+
+//switch case
+
+function basicOp(operation, value1, value2) {
+  switch (operation) {
+    case "+":
+      return value1 + value2
+    case "-":
+      return value1 - value2
+    case "*":
+      return value1 * value2
+    case "/":
+      return value1 / value2
+    default:
+      return 0
+  }
+}
