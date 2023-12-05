61d96ee076c5b42ff96e5ac9b55dd5a3271b8e14
Arkadiy <alibullterrier@gmail.com>
2023-12-05 11:51:19 +0200

    digitize, for cycle

--- js-5.js
+++ js-5.js
@@ -34,3 +34,12 @@ function digitize(n) {
       return parseInt(i)
     })
 }
+
+function digitize(n) {
+  let str = n + ""
+  let arr = []
+  for (let i = str.length - 1; i >= 0; i--) arr.push(+str[i])
+  return arr
+}
+
+//--------------------------------------------------------------
