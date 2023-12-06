561776e02a7cf212535d0d9a33c175fea5e94a53
Arkadiy <alibullterrier@gmail.com>
2023-12-06 06:05:43 +0200

    isDivisible

--- js-5.js
+++ js-5.js
@@ -11,3 +11,39 @@ const digitize = (num) =>
     .split("")
     .reverse()
     .map((el) => +el)
+
+function digitize(n) {
+  return Array.from(String(n), Number).reverse()
+}
+
+const digitize = (x) => {
+  x = `${x}`.split("").reverse()
+  let a = []
+  for (let i = 0; i < x.length; i++) {
+    a.push(+x[i])
+  }
+  return a
+}
+
+function digitize(n) {
+  return n
+    .toString()
+    .split("")
+    .reverse()
+    .map(function (i) {
+      return parseInt(i)
+    })
+}
+
+function digitize(n) {
+  let str = n + ""
+  let arr = []
+  for (let i = str.length - 1; i >= 0; i--) arr.push(+str[i])
+  return arr
+}
+
+//--------------------------------------------------------------
+
+function isDivisible(n, x, y) {
+  return n % x === 0 && n % y === 0
+}
