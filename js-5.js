6c00dd49b0e222e9ee49e701550c72e289943571
Arkadiy <alibullterrier@gmail.com>
2023-12-06 06:06:19 +0200

    isDivisible if/else

--- js-5.js
+++ js-5.js
@@ -49,3 +49,10 @@ function isDivisible(n, x, y) {
 }
 
 const isDivisible = (n, x, y) => n % x === 0 && n % y === 0
+
+function isDivisible(n, x, y) {
+  if (n % x === 0 && n % y === 0) {
+    return true
+  }
+  return false
+}
