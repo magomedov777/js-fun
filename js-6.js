a3dc3b5b1c5f2cd8d33c9bfa346d45e718521f29
Arkadiy <alibullterrier@gmail.com>
2023-12-07 09:32:04 +0200

    work with map, cycle for

--- js-6.js
+++ js-6.js
@@ -4,3 +4,12 @@ maps = (x) => x.map((el) => el * 2)
 function maps(x) {
   return x.map((el) => el * 2)
 }
+
+//with cycle for
+function maps(x) {
+  let arr = []
+  for (let i = 0; i < x.length; i++) {
+    arr.push(x[i] * 2)
+  }
+  return arr
+}
