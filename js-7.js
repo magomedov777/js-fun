00e6c63155dcecea2817983f428199c536121569
Arkadiy <alibullterrier@gmail.com>
2023-12-08 08:55:41 +0200

    find average, like

--- js-7.js
+++ js-7.js
@@ -75,3 +75,13 @@ function find_average(array) {
 //other solution
 const find_average = (array) =>
   array.reduce((acc, curr) => acc + curr, 0) / array.length || 0
+
+//like!
+function find_average(array) {
+  if (array.length == 0) return 0
+  let result = 0
+  for (num of array) {
+    result += num
+  }
+  return result / array.length
+}
