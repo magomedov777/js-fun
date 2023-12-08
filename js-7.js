e5ea2d20bebe8c1919cfc18be478ad3205244a4f
Arkadiy <alibullterrier@gmail.com>
2023-12-08 08:55:13 +0200

    find average, cycle for

--- js-7.js
+++ js-7.js
@@ -59,3 +59,15 @@ const find_average = (array) => {
     ? 0
     : array.reduce((acc, ind) => acc + ind, 0) / array.length
 }
+
+//with cycle for
+function find_average(array) {
+  if (array.length === 0) {
+    return 0
+  }
+  let result = 0
+  for (i = 0; i < array.length; i++) {
+    result += array[i]
+  }
+  return result / array.length
+}
