f9caee6c47b35b9b4e20203dfc3928153618f36c
Arkadiy <alibullterrier@gmail.com>
2023-12-09 00:01:21 +0200

    sum arrays, easy

--- js-8.js
+++ js-8.js
@@ -1,3 +1,8 @@
 //Sum Arrays
 //first solution
 sum = (numbers) => (!numbers ? 0 : numbers.reduce((acc, curr) => acc + curr, 0))
+
+//easy
+function sum(numbers) {
+  return numbers.reduce((a, b) => a + b, 0)
+}
