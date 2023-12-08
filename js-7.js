8c5b6c9f8989e5dd3591ef9921ec38acaabd58fc
Arkadiy <alibullterrier@gmail.com>
2023-12-08 08:53:21 +0200

    new solution

--- js-7.js
+++ js-7.js
@@ -4,3 +4,12 @@ function areYouPlayingBanjo(name) {
     return `${name} plays banjo`
   } else return `${name} does not play banjo`
 }
+
+//new solution
+function areYouPlayingBanjo(name) {
+  return (
+    name +
+    (name[0].toLowerCase() == "r" ? " plays" : " does not play") +
+    " banjo"
+  )
+}
