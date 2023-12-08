f67f462726219c3d208c46f2acb33901f05644d0
Arkadiy <alibullterrier@gmail.com>
2023-12-09 00:05:01 +0200

    count positives, sum negatives, if/else

--- js-8.js
+++ js-8.js
@@ -108,3 +108,16 @@ function countPositivesSumNegatives(input) {
       ]
     : []
 }
+
+//if/else
+function countPositivesSumNegatives(input) {
+  if (!Array.isArray(input) || !input.length) return []
+  return input.reduce(
+    (arr, n) => {
+      if (n > 0) arr[0]++
+      if (n < 0) arr[1] += n
+      return arr
+    },
+    [0, 0]
+  )
+}
