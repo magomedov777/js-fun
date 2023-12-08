7a225a48177d4aae6bbfdaa602efdd2db61cdcbb
Arkadiy <alibullterrier@gmail.com>
2023-12-09 00:04:48 +0200

    count positives, sum negatives, one line

--- js-8.js
+++ js-8.js
@@ -98,3 +98,13 @@ function countPositivesSumNegatives(input) {
   }
   return [positive, negative]
 }
+
+//one line
+function countPositivesSumNegatives(input) {
+  return input && input.length
+    ? [
+        input.filter((p) => p > 0).length,
+        input.filter((n) => n < 0).reduce((a, b) => a + b, 0),
+      ]
+    : []
+}
