6ec317f8c421e053f578407c9649ccddc0b3ac51
Arkadiy <alibullterrier@gmail.com>
2023-12-09 00:04:23 +0200

    count positives, sum negatives

--- js-8.js
+++ js-8.js
@@ -74,3 +74,15 @@ function simpleMultiplication(number) {
       return number * 9
   }
 }
+
+//-------------------------------
+//Count of positives / sum of negatives
+function countPositivesSumNegatives(input) {
+  if (!input || input.length === 0) {
+    return []
+  }
+  let arr = []
+  arr.push(input.filter((el) => el > 0).length)
+  arr.push(input.filter((el) => el < 0).reduce((acc, curr) => acc + curr, 0))
+  return arr
+}
