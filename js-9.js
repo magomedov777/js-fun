edd3dd488b0e63178238cf8db3ea9831aba4cd07
Arkadiy <alibullterrier@gmail.com>
2023-12-11 05:54:05 +0200

    check func, filter

--- js-9.js
+++ js-9.js
@@ -11,3 +11,9 @@ function check(a, x) {
 function check(a, x) {
   return a.indexOf(x) > -1 ? true : false
 }
+
+//with filter
+function check(a, x) {
+  let b = a.filter((el) => el == x)
+  return b.length ? true : false
+}
