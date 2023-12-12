a21a4cd171641b88684e7c3f464ed7a587dabf7b
Arkadiy <alibullterrier@gmail.com>
2023-12-12 09:32:19 +0200

    calculate BMI, switch/case

--- js-10.js
+++ js-10.js
@@ -17,3 +17,18 @@ const bmi = (w, h, bmi = w / h / h) =>
     : bmi <= 30
     ? "Overweight"
     : "Obese"
+
+//switch case
+function bmi(weight, height) {
+  var formula = weight / Math.pow(height, 2)
+  switch (true) {
+    case formula <= 18.5:
+      return "Underweight"
+    case formula <= 25.0:
+      return "Normal"
+    case formula <= 30:
+      return "Overweight"
+    default:
+      return "Obese"
+  }
+}
