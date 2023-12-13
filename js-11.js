034d59563ec07f2a68fd70a07c45afb34f6ca1b4
Arkadiy <alibullterrier@gmail.com>
2023-12-13 05:32:34 +0200

    greet func, new solution

--- js-11.js
+++ js-11.js
@@ -57,3 +57,8 @@ greet = (name) => (name === "Johnny" ? "Hello, my love!" : `Hello, ${name}!`)
 function greet(name) {
   return "Hello, " + (name == "Johnny" ? "my love" : name) + "!"
 }
+
+function greet(name) {
+  if (name === "Johnny") return "Hello, my love!"
+  return "Hello, " + name + "!"
+}
