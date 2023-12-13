ab7ea8216b6a1492caed260d768ef19f5bda4b22
Arkadiy <alibullterrier@gmail.com>
2023-12-13 05:31:26 +0200

    reversed word, cycle for solution

--- js-11.js
+++ js-11.js
@@ -29,3 +29,13 @@ function countBy(x, n) {
 
 //reversed words
 reverseWords = (str) => str.split(" ").reverse().join(" ")
+
+//with cycle for
+function reverseWords(str) {
+  let reverse = []
+  let words = str.split(" ")
+  for (let i = words.length - 1; i >= 0; i--) {
+    reverse.push(words[i])
+  }
+  return reverse.join(" ")
+}
