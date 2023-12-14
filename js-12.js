b790531d213e124eb37ffe9806d4e4efc30bc332
Arkadiy <alibullterrier@gmail.com>
2023-12-14 09:53:25 +0200

    rps, good

--- js-12.js
+++ js-12.js
@@ -14,3 +14,14 @@ const rps = (p1, p2) => {
   if (p1 === rock && p2 === scissors) return "Player 1 won!"
   if (p1 === scissors && p2 === rock) return "Player 2 won!"
 }
+
+//good solution
+function rps(p1, p2) {
+  if (p1 === p2) return "Draw!"
+  const rules = { rock: "scissors", paper: "rock", scissors: "paper" }
+  if (p2 === rules[p1]) {
+    return "Player 1 won!"
+  } else {
+    return "Player 2 won!"
+  }
+}
