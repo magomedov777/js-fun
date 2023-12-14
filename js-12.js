a50e818096c1c617b8f89bd32fcc6fc6c36a3180
Arkadiy <alibullterrier@gmail.com>
2023-12-14 09:53:08 +0200

    rps, first solution

--- js-12.js
+++ js-12.js
@@ -1,1 +1,16 @@
 //new lesson
+//first
+const rps = (p1, p2) => {
+  let scissors = "scissors"
+  let paper = "paper"
+  let rock = "rock"
+  if (p1 === scissors && p2 === scissors) return "Draw!"
+  if (p1 === paper && p2 === paper) return "Draw!"
+  if (p1 === rock && p2 === rock) return "Draw!"
+  if (p1 === scissors && p2 === paper) return "Player 1 won!"
+  if (p1 === paper && p2 === scissors) return "Player 2 won!"
+  if (p1 === paper && p2 === rock) return "Player 1 won!"
+  if (p1 === rock && p2 === paper) return "Player 2 won!"
+  if (p1 === rock && p2 === scissors) return "Player 1 won!"
+  if (p1 === scissors && p2 === rock) return "Player 2 won!"
+}
