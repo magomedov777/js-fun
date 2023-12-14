fedc9a9814ead8d8c8e58088055e04dea0cafbdb
Arkadiy <alibullterrier@gmail.com>
2023-12-14 09:56:04 +0200

    grade first

--- js-12.js
+++ js-12.js
@@ -116,3 +116,13 @@ function points(games) {
 }
 
 //---------------------------------------
+//get grade func
+//first
+function getGrade(s1, s2, s3) {
+  let score = (s1 + s2 + s3) / 3
+  if (score >= 90 && score <= 100) return "A"
+  if (score >= 80 && score < 90) return "B"
+  if (score >= 70 && score < 80) return "C"
+  if (score >= 60 && score < 70) return "D"
+  if (0 <= score < 60) return "F"
+}
