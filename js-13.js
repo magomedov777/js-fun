e613bf019c07e1e78bbb1ac00573fed0ab02a858
Arkadiy <alibullterrier@gmail.com>
2023-12-15 05:45:16 +0200

    area or perimetr, if/else

--- js-13.js
+++ js-13.js
@@ -3,3 +3,11 @@
 
 //first
 areaOrPerimeter = (l, w) => (l !== w ? (l + w) * 2 : l * w)
+
+//if/else
+function areaOrPerimeter(l, w) {
+  if (l !== w) {
+    return (l + w) * 2
+  }
+  return l * w
+}
