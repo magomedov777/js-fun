1127f1dc5354b62efbf1b5691d8cb4b953d8e00a
Arkadiy <alibullterrier@gmail.com>
2023-12-15 05:45:27 +0200

    area or perimetr, base

--- js-13.js
+++ js-13.js
@@ -11,3 +11,10 @@ function areaOrPerimeter(l, w) {
   }
   return l * w
 }
+
+//base
+const areaOrPerimeter = function (l, w) {
+  let area = l * w
+  let perimeter = (l + w) * 2
+  return l === w ? area : perimeter
+}
