72c4f7e31f944b43950afd78113df6008198f063
Arkadiy <alibullterrier@gmail.com>
2023-12-16 00:04:38 +0200

    quarterOf, switch/case

--- js-14.js
+++ js-14.js
@@ -111,3 +111,25 @@ const quarterOf = (month) => {
 
 //short
 const _quarterOf = (m) => Math.ceil(m / 3)
+
+//switch/case | just for fun
+const __quarterOf = (month, quarter = 4) => {
+  switch (month) {
+    case 1:
+    case 2:
+    case 3:
+      quarter = 1
+      break
+    case 4:
+    case 5:
+    case 6:
+      quarter = 2
+      break
+    case 7:
+    case 8:
+    case 9:
+      quarter = 3
+      break
+  }
+  return quarter
+}
