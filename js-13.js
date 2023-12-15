ba86c52a307dcdac2017f4a5d24974b9cc5020a9
Arkadiy <alibullterrier@gmail.com>
2023-12-15 05:46:31 +0200

    new task, first

--- js-13.js
+++ js-13.js
@@ -37,3 +37,11 @@ function greet(name, owner) {
   }
   return "Hello guest"
 }
+
+//new task
+//first
+const feast = (beast, dish) => {
+  return (
+    beast[0] === dish[0] && beast[beast.length - 1] === dish[dish.length - 1]
+  )
+}
