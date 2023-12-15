fbe6c67a68e3ab8f2baf8f0e182d0ed12160a053
Arkadiy <alibullterrier@gmail.com>
2023-12-16 00:01:35 +0200

    first task, new

--- js-14.js
+++ js-14.js
@@ -15,3 +15,16 @@ const listSquared = (x, y) => {
   }
   return array
 }
+
+//new
+const _listSquared = (m, n) => {
+  const res = []
+  for (let i = m; i <= n; i++) {
+    let sum = 0
+    for (let j = 1; j <= n; j++) {
+      if (!(i % j)) sum += j ** 2
+    }
+    if (Number.isInteger(sum ** 0.5)) res.push([i, sum])
+  }
+  return res
+}
