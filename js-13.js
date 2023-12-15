d0a6d0f6bb1f0997b933e3ea1c5464a2e0dbd9d9
Arkadiy <alibullterrier@gmail.com>
2023-12-15 05:48:11 +0200

    count task, like, four condition

--- js-13.js
+++ js-13.js
@@ -77,3 +77,9 @@ function monkeyCount(n) {
 function monkeyCount(n) {
   return Array.from({ length: n }, (_, i) => i + 1)
 }
+
+//like | four condition
+function monkeyCount(n) {
+  for (let i = 0, arr = []; i < n; arr.push(++i));
+  return arr
+}
