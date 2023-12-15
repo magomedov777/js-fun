365b864a5c3a03ae54ade4e458a28a77c3132177
Arkadiy <alibullterrier@gmail.com>
2023-12-16 00:01:22 +0200

    first task

--- js-14.js
+++ js-14.js
@@ -1,1 +1,17 @@
 //new lesson
+//first task
+const listSquared = (x, y) => {
+  let array = []
+  for (let i = x; i <= y; i++) {
+    let result = 0
+    for (let j = 1; j <= i; j++) {
+      if (i % j === 0) {
+        result += j * j
+      }
+    }
+    if (Math.sqrt(result) % 1 === 0) {
+      array.push([i, result])
+    }
+  }
+  return array
+}
