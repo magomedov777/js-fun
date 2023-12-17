bdb306f20d3a702d7ec4b4e0f4a8f82c2e51c980
Arkadiy <alibullterrier@gmail.com>
2023-12-17 12:12:46 +0200

    take func, cycle for

--- js-15.js
+++ js-15.js
@@ -17,3 +17,16 @@ function take(arr, n) {
 function take(arr, n) {
   return arr.filter((elem, i) => i < n)
 }
+
+//with cycle for
+function take(arr, n) {
+  let counter = 0
+  let res = []
+  for (let i of arr) {
+    counter += 1
+    if (counter <= n && counter <= arr.length) {
+      res.push(i)
+    }
+  }
+  return res
+}
