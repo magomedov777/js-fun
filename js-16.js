8d04c5258396088135a9a2ad98cfaaade7b43c82
Arkadiy <alibullterrier@gmail.com>
2023-12-18 06:04:20 +0200

    new task, best practices

--- js-16.js
+++ js-16.js
@@ -2,3 +2,8 @@
 //fundamental | alghoritms
 //first task
 enough = (cap, on, wait) => (cap - on >= wait ? 0 : Math.abs(cap - on - wait))
+
+//best practices
+function enough(cap, on, wait) {
+  return Math.max(wait + on - cap, 0)
+}
