411d4c2a39aa51a090d7da45129160b6b60c5db7
Arkadiy <alibullterrier@gmail.com>
2023-12-19 10:24:26 +0200

    move zeros, first

--- js-17.js
+++ js-17.js
@@ -1,1 +1,14 @@
 //new lesson
+//first task
+function moveZeros(arr) {
+  let withoutZeros = []
+  let zeros = []
+  for (let num of arr) {
+    if (num !== 0) {
+      withoutZeros.push(num)
+    } else {
+      zeros.push(num)
+    }
+  }
+  return withoutZeros.concat(zeros)
+}
