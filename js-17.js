361bb01bd31d802284246673c5fc40324446c61d
Arkadiy <alibullterrier@gmail.com>
2023-12-19 10:27:18 +0200

    strCount, first

--- js-17.js
+++ js-17.js
@@ -93,3 +93,15 @@ function pigIt(str) {
 }
 
 //--------------------------------------------------
+
+//new task
+//firs
+function strCount(str, letter) {
+  let res = 0
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === letter) {
+      res++
+    }
+  }
+  return res
+}
