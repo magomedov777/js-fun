5c93b5e5a67d41649587ec8d93f960ff5f26e0d9
Arkadiy <alibullterrier@gmail.com>
2023-12-20 06:03:05 +0200

    volume of cuboid, with reduce

--- js-18.js
+++ js-18.js
@@ -14,3 +14,10 @@ class Kata {
     return length * width * height;
   }
 }
+
+//with reduce
+class Kata {
+  static getVolumeOfCuboid(length, width, height) {
+    return Array.prototype.slice.call(arguments).reduce((a, b) => a * b);
+  }
+}
