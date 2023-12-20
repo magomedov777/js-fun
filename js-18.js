49c34aeee60349387c6bef8b2600f60faa327154
Arkadiy <alibullterrier@gmail.com>
2023-12-20 06:04:49 +0200

    new task, first

--- js-18.js
+++ js-18.js
@@ -107,3 +107,10 @@ function switchItUp(number) {
 }
 
 //------------------------------------------
+//new task
+//string
+//first
+const correct = (string) => {
+  let newStr = string.replace(/5/g, "S").replace(/0/g, "O").replace(/1/g, "I");
+  return newStr;
+};
