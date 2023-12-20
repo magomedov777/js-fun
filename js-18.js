bed0aaeb1a223c6725408fd42bce7aa5ef684a42
Arkadiy <alibullterrier@gmail.com>
2023-12-20 06:04:03 +0200

    switsh up, switch/case

--- js-18.js
+++ js-18.js
@@ -49,3 +49,31 @@ function switchItUp(number) {
 //smart
 switchItUp = (n) =>
   ["Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"][n];
+
+//switch/case
+function switchItUp(number) {
+  switch (number) {
+    case 0:
+      return "Zero";
+    case 1:
+      return "One";
+    case 2:
+      return "Two";
+    case 3:
+      return "Three";
+    case 4:
+      return "Four";
+    case 5:
+      return "Five";
+    case 6:
+      return "Six";
+    case 7:
+      return "Seven";
+    case 8:
+      return "Eight";
+    case 9:
+      return "Nine";
+    default:
+      return "Unknown number";
+  }
+}
