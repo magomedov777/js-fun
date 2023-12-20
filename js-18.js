922041fbbf4f800b72750d7d5e1df9a0911cd950
Arkadiy <alibullterrier@gmail.com>
2023-12-20 06:04:33 +0200

    split & obj

--- js-18.js
+++ js-18.js
@@ -83,3 +83,27 @@ function switchItUp(number) {
   const words = ["Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"];
   return words[number];
 }
+
+//with split
+function switchItUp(number) {
+  return "Zero One Two Three Four Five Six Seven Eight Nine".split(" ")[number];
+}
+
+//across object
+function switchItUp(number) {
+  let nums = {
+    0: "Zero",
+    1: "One",
+    2: "Two",
+    3: "Three",
+    4: "Four",
+    5: "Five",
+    6: "Six",
+    7: "Seven",
+    8: "Eight",
+    9: "Nine",
+  };
+  return nums[number];
+}
+
+//------------------------------------------
