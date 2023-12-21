3ed349fd7fa93fbecf158e5dae6a7d17090b1429
Arkadiy <alibullterrier@gmail.com>
2023-12-21 10:08:54 +0200

    dogYears, switch/case

--- js-19.js
+++ js-19.js
@@ -78,3 +78,15 @@ const humanYearsCatYearsDogYears = (humanYears) => {
   }
   return [humanYears, catYears, dogYears];
 };
+
+//switch/case
+function humanYearsCatYearsDogYears(humanYears) {
+  switch (humanYears) {
+    case 1:
+      return [1, 15, 15];
+    case 2:
+      return [2, 24, 24];
+    default:
+      return [humanYears, (humanYears - 2) * 4 + 24, (humanYears - 2) * 5 + 24];
+  }
+}
