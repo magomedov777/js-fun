e792f1919cdc0fac690e5aeab9c09ac1177ddd09
Arkadiy <alibullterrier@gmail.com>
2023-12-21 10:08:40 +0200

    dogYears, smart

--- js-19.js
+++ js-19.js
@@ -58,3 +58,23 @@ function humanYearsCatYearsDogYears(humanYears) {
   if (humanYears === 2) return [2, 24, 24];
   return [humanYears, 24 + (humanYears - 2) * 4, 24 + (humanYears - 2) * 5];
 }
+
+//smart
+const humanYearsCatYearsDogYears = (humanYears) => {
+  let catYears = 0;
+  let dogYears = 0;
+
+  for (let i = 1; i <= humanYears; i++) {
+    if (i === 1) {
+      catYears += 15;
+      dogYears += 15;
+    } else if (i === 2) {
+      catYears += 9;
+      dogYears += 9;
+    } else {
+      catYears += 4;
+      dogYears += 5;
+    }
+  }
+  return [humanYears, catYears, dogYears];
+};
