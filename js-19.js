7d3e97db67af2a03406a8615d3338e6694ec8d2d
Arkadiy <alibullterrier@gmail.com>
2023-12-21 10:08:28 +0200

    dogYears, first

--- js-19.js
+++ js-19.js
@@ -49,3 +49,12 @@ function getAge(inputString) {
 function getAge(inputString) {
   return inputString.match(/[0-9]/)[0] * 1;
 }
+
+//-----------------------------------------
+//new task | dog years
+//first
+function humanYearsCatYearsDogYears(humanYears) {
+  if (humanYears === 1) return [1, 15, 15];
+  if (humanYears === 2) return [2, 24, 24];
+  return [humanYears, 24 + (humanYears - 2) * 4, 24 + (humanYears - 2) * 5];
+}
