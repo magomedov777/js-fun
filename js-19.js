2e267668b2855eaedfe9178d58f7c348d7dcde59
Arkadiy <alibullterrier@gmail.com>
2023-12-21 10:07:57 +0200

    first task, switch/case

--- js-19.js
+++ js-19.js
@@ -18,3 +18,29 @@ function getAge(inputString) {
 }
 
 getAge = (inputString) => parseInt(inputString[0] + "years old");
+
+//switch/case | for fun
+function getAge(inputString) {
+  switch (inputString) {
+    case "1 years old":
+      return 1;
+    case "2 years old":
+      return 2;
+    case "3 years old":
+      return 3;
+    case "4 years old":
+      return 4;
+    case "5 years old":
+      return 5;
+    case "6 years old":
+      return 6;
+    case "7 years old":
+      return 7;
+    case "8 years old":
+      return 8;
+    case "9 years old":
+      return 9;
+    default:
+      return 1;
+  }
+}
