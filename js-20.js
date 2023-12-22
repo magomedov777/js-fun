1132bfe6fc834bb538dd0073d08614bb60e9e1a5
Arkadiy <alibullterrier@gmail.com>
2023-12-22 05:58:42 +0200

    is it palindrome, with while

--- js-20.js
+++ js-20.js
@@ -2,3 +2,16 @@
 //palindrome task
 //first
 isPalindrome = (x) => x.split("").reverse().join("").toLowerCase() === x.toLowerCase();
+
+//with cycle while
+function isPalindrome(x) {
+  x = x.toLowerCase();
+  let a = 0;
+  let b = x.length;
+  while (a < b) {
+    if (x[a] != x[b - 1]) return false;
+    a++;
+    b--;
+  }
+  return true;
+}
