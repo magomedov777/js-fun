48666cb42c6725836dc4ac177961a40ee055029c
Arkadiy <alibullterrier@gmail.com>
2023-12-23 00:06:57 +0200

    new task, with cycle for

--- js-21.js
+++ js-21.js
@@ -85,3 +85,14 @@ divisibleBy = (numbers, divisor) => numbers.filter((el) => el % divisor === 0);
 function divisibleBy(numbers, divisor) {
   return numbers.filter((n) => n % divisor === 0);
 }
+
+//with cycle for
+function divisibleBy(numbers, divisor) {
+  let newArr = [];
+  for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] % divisor === 0) {
+      newArr.push(numbers[i]);
+    }
+  }
+  return newArr;
+}
