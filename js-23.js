9973bd62980123bc58047f3e5fbc9da7fbd003b4
Arkadiy <alibullterrier@gmail.com>
2023-12-25 06:14:14 +0200

    final grade, switch/case

--- js-23.js
+++ js-23.js
@@ -74,3 +74,17 @@ function finalGrade(exam, projects) {
 const finalGrade = (exam, projects) => {
   return exam > 90 || projects > 10 ? 100 : exam > 75 && projects >= 5 ? 90 : exam > 50 && projects >= 2 ? 75 : 0;
 };
+
+//witch/case
+function finalGrade(exam, projects) {
+  switch (true) {
+    case exam > 90 || projects > 10:
+      return 100;
+    case exam > 75 && projects >= 5:
+      return 90;
+    case exam > 50 && projects >= 2:
+      return 75;
+    default:
+      return 0;
+  }
+}
