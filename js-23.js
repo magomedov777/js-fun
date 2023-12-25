719db95775bf76aa66b79e98c8edc3910c2a3b99
Arkadiy <alibullterrier@gmail.com>
2023-12-25 06:12:20 +0200

    find difference, good

--- js-23.js
+++ js-23.js
@@ -3,3 +3,8 @@
 //first
 const findDifference = (a, b) =>
   Math.abs(a.reduce((prev, curr) => prev * curr) - b.reduce((prev, curr) => prev * curr));
+
+//good
+function find_difference(a, b) {
+  return Math.abs(a[0] * a[1] * a[2] - b[0] * b[1] * b[2]);
+}
