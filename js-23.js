e9fbbc2039b5c4fd6c77542afde81afdf6519aa3
Arkadiy <alibullterrier@gmail.com>
2023-12-25 06:12:11 +0200

    find difference, first

--- js-23.js
+++ js-23.js
@@ -1,2 +1,5 @@
 //new lesson | sunday
 //difference of volumes of cuboids
+//first
+const findDifference = (a, b) =>
+  Math.abs(a.reduce((prev, curr) => prev * curr) - b.reduce((prev, curr) => prev * curr));
