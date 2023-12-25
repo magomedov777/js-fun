cd5b17ed61f8f0a5512d5acf522b6ac9b9b458f3
Arkadiy <alibullterrier@gmail.com>
2023-12-25 06:13:45 +0200

    final grade, first

--- js-23.js
+++ js-23.js
@@ -59,3 +59,13 @@ function findDifference(a, b) {
   }
   return res;
 }
+
+//----------------------------------------------------
+//new task | final grade
+//first
+function finalGrade(exam, projects) {
+  if (exam > 90 || projects > 10) return 100;
+  if (exam > 75 && projects > 4) return 90;
+  if (exam > 50 && projects > 1) return 75;
+  return 0;
+}
