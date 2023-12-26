8979007b01c104a31af64dcb6ab2031c77569c32
Arkadiy <alibullterrier@gmail.com>
2023-12-26 09:27:19 +0200

    easy, first

--- js-24.js
+++ js-24.js
@@ -26,3 +26,14 @@ function goals(laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
   let result = laLigaGoals + copaDelReyGoals + championsLeagueGoals;
   return result;
 }
+
+//---------------------------------------------------------
+//first
+function howMuchILoveYou(nbPetals) {
+  let x = ["I love you", "a little", "a lot", "passionately", "madly", "not at all"];
+  let y = (nbPetals % 6) - 1;
+  if (y === -1) {
+    y = x.length - 1;
+  }
+  return x[y];
+}
