6f3ad3cc2643c2f1babbb3355bfb44dd5ebe253d
Arkadiy <alibullterrier@gmail.com>
2023-12-26 09:27:29 +0200

    easy, base

--- js-24.js
+++ js-24.js
@@ -37,3 +37,16 @@ function howMuchILoveYou(nbPetals) {
   }
   return x[y];
 }
+
+//base
+function howMuchILoveYou(nbPetals) {
+  let phrase = {
+    0: "not at all",
+    1: "I love you",
+    2: "a little",
+    3: "a lot",
+    4: "passionately",
+    5: "madly",
+  };
+  return phrase[nbPetals % 6];
+}
