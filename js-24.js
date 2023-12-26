d7da999e11b511c810b9050b2a767822380cecd3
Arkadiy <alibullterrier@gmail.com>
2023-12-26 09:27:56 +0200

    easy, switch/case

--- js-24.js
+++ js-24.js
@@ -53,3 +53,21 @@ function howMuchILoveYou(nbPetals) {
 
 //one line
 const howMuchILoveYou = (n) => ["not at all", "I love you", "a little", "a lot", "passionately", "madly"][n % 6];
+
+//switch/case
+function howMuchILoveYou(nbPetals) {
+  switch ((nbPetals - 1) % 6) {
+    case 1:
+      return "a little";
+    case 2:
+      return "a lot";
+    case 3:
+      return "passionately";
+    case 4:
+      return "madly";
+    case 5:
+      return "not at all";
+    default:
+      return "I love you";
+  }
+}
