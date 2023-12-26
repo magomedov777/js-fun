26d5ed07029da463a0844e8b0b2a26f986fe4a87
Arkadiy <alibullterrier@gmail.com>
2023-12-26 09:26:35 +0200

    goals, step by step

--- js-24.js
+++ js-24.js
@@ -6,3 +6,9 @@ function goals(laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
 
 //smart
 const goals = (...a) => a.reduce((s, v) => s + v, 0);
+
+//step by step
+function goals(laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+  let goals = [laLigaGoals, copaDelReyGoals, championsLeagueGoals];
+  return goals.reduce((a, b) => a + b, 0);
+}
