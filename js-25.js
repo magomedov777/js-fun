dd49290fd28ec7fdf27f42c13f0077b413784598
Arkadiy <alibullterrier@gmail.com>
2023-12-27 06:05:04 +0200

    square root, base

--- js-25.js
+++ js-25.js
@@ -2,3 +2,11 @@
 //To square or not to square
 //first
 squareOrSquareRoot = (array) => array.map((el) => (Math.sqrt(el) % 1 ? el * el : Math.sqrt(el)));
+
+//base
+function squareOrSquareRoot(array) {
+  return array.map((x) => {
+    const r = Math.sqrt(x);
+    return r % 1 == 0 ? r : x * x;
+  });
+}
