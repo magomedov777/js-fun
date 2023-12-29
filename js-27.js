9625db38cf40bb2c97d7f67a7d28025ae3dc6dfc
Arkadiy <alibullterrier@gmail.com>
2023-12-29 05:49:46 +0200

    bug fixing, first

--- js-27.js
+++ js-27.js
@@ -1,1 +1,10 @@
 //new lesson | thirsday
+//bug fixing
+//first
+function createArray(number) {
+  var newArray = [];
+  for (var counter = 1; counter <= number; ) {
+    newArray.push(counter++);
+  }
+  return newArray;
+}
