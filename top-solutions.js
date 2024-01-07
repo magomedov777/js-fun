f6dc20cc1725b013c79568a3f7c5db8cc0201d67
Arkadiy <alibullterrier@gmail.com>
2024-01-07 16:00:31 +0200

    top solutions + 1

--- top-solutions.js
+++ top-solutions.js
@@ -46,3 +46,13 @@ function sumStrings(a, b) {
   res = res.replace(/^[0]*/g, "");
   return res;
 }
+
+function findMultiples(integer, limit) {
+  let arr = [];
+  for (let i = 1; i <= limit; i++) {
+    if (i % integer === 0) {
+      arr.push(i);
+    }
+  }
+  return arr;
+}
