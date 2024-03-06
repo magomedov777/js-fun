84e4610c84aebb3cd63a46849abe9d1cca6bf3d4
Arkadiy <alibullterrier@gmail.com>
2024-03-06 06:09:22 +0200

    top-solutions++

--- top-solutions/top-solutions.js
+++ top-solutions/top-solutions.js
@@ -491,3 +491,15 @@ function logs(x, a, b) {
   }
   return Infinity
 }
+
+function flickSwitch(arr) {
+  let result = []
+  let switchValue = false
+  for (let item of arr) {
+    if (item === 'flick') {
+      switchValue = !switchValue
+    }
+    result.push(!switchValue)
+  }
+  return result
+}
