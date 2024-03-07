63a48753b894b1da275dfebe0466f6b0e43b9892
Arkadiy <alibullterrier@gmail.com>
2024-03-07 08:53:25 +0200

    new lesson

---
+++ 61-70 lessons/js-69.js
@@ -0,0 +1,65 @@
+//new lesson | thursday
+//order operations
+//first
+orderOperations = () => (2 + 2 * 2 + 2) ** 2 / 2
+
+//base
+function orderOperations() {
+  return (2 + 2) * (2 + 2) * 2
+}
+
+//like
+function orderOperations() {
+  return 2 + 2 * 2 + 2 * 2 + 22
+}
+
+//good
+orderOperations = () => (2 + 2 * 2 + 2) * 2 * 2
+
+//<<
+orderOperations = (
+  a //+*+*
+) => (2 << 2) << 2
+
+//fun
+orderOperations = () => '+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*'.length
+
+//fun
+function orderOperations() {
+  let arr = []
+  arr.push('a')
+  arr.push('a')
+  arr.push('a')
+  arr.push('a')
+  arr.push('a')
+  arr.push('a')
+  arr.push('a')
+  arr.push('a')
+  arr.push('a')
+  arr.push('a')
+  arr.push('a')
+  arr.push('a')
+  arr.push('a')
+  arr.push('a')
+  arr.push('a')
+  arr.push('a')
+  arr.push('a')
+  arr.push('a')
+  arr.push('a')
+  arr.push('a')
+  arr.push('a')
+  arr.push('a')
+  arr.push('a')
+  arr.push('a')
+  arr.push('a')
+  arr.push('a')
+  arr.push('a')
+  arr.push('a')
+  arr.push('a')
+  arr.push('a')
+  arr.push('a')
+  arr.push('a')
+
+  //   return (2 + 2) * (2 + 2) * 2
+  return arr.length
+}
