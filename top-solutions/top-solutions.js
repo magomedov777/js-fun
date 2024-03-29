637bd4b02d18d50a1c510cbc1cf9a837a7f5d83b
Arkadiy <alibullterrier@gmail.com>
2024-03-30 00:01:00 +0200

    top-solution++

--- top-solutions/top-solutions.js
+++ top-solutions/top-solutions.js
@@ -634,3 +634,69 @@ const factorial = (x) => (x <= 1 ? 1 : x * factorial(x - 1))
 function amIWilson(p) {
   return p === 5 || p === 13 || p === 563 ? true : false
 }
+
+let result = []
+function sum(n, m = n) {
+  if (n == 0) return 1
+  if (n < 0 || m == 0) return 0
+  if (result[n] && result[n][m]) return result[n][m]
+  let comp = sum(n, m - 1) + sum(n - m, m)
+  if (!result[n]) {
+    result[n] = []
+  }
+  result[n][m] = comp
+  return comp
+}
+
+function sum(num) {
+  const memo = {}
+  function sumRecursive(target, max) {
+    let partitions = 0
+    for (let i = Math.min(target, max); i > 0; i--) {
+      if (i === target || i === 1) partitions++
+      else
+        partitions += memo[`${target - i}-${i}`] || sumRecursive(target - i, i)
+    }
+    memo[`${target}-${max}`] = partitions
+    return partitions
+  }
+  return sumRecursive(num, num)
+}
+
+function sum(n) {
+  this.mem = this.mem || [1]
+  if (n < 0) return 0
+  if (this.mem[n]) return this.mem[n]
+  this.mem[n] = 0
+  for (let i = 1; i <= n; i++)
+    this.mem[n] +=
+      (i % 2 ? 1 : -1) *
+      (sum(n - (i * (3 * i - 1)) / 2) + sum(n - (i * (3 * i + 1)) / 2))
+  return this.mem[n]
+}
+
+const _sum = (num) => {
+  const dp = [1, ...new Array(num).fill(0)]
+  for (let i = 1; i <= num; i++) {
+    for (let j = i; j <= num; j++) {
+      dp[j] += dp[j - i]
+    }
+  }
+  return dp[num]
+}
+
+let pointsPer48 = (ppg, mpg) =>
+  ppg === 0 && mpg === 0 ? 0 : +((ppg / mpg) * 48).toFixed(1)
+
+function pointsPer48(ppg, mpg) {
+  return +((ppg / mpg) * 48).toFixed(1) || 0
+}
+
+const pointsPer48 = (ppg, mpg) => (mpg ? +((ppg * 48) / mpg).toFixed(1) : 0)
+
+function pointsPer48(ppg, mpg) {
+  return mpg ? Number((ppg * (48 / mpg)).toFixed(1)) : 0
+}
+
+let pointsPer48 = (ppg, mpg) =>
+  mpg == 0 ? 0 : Math.round((ppg / mpg) * 48 * 10) / 10
