4384cb1b3a54249ac9a31a27bbcec527fde197ee
Arkadiy <alibullterrier@gmail.com>
2024-04-01 06:04:37 +0300

    top-solution++

--- top-solutions/top-solutions.js
+++ top-solutions/top-solutions.js
@@ -733,3 +733,27 @@ const nextItem = (xs, item) => {
     if (el === item) return iter.next().value
   }
 }
+
+function iceBrickVolume(radius, bottleLength, rimLength) {
+  let length = bottleLength - rimLength
+  let side = Math.sqrt(radius * radius + radius * radius)
+  return Math.round(side * side * length)
+}
+
+iceBrickVolume = (radius, bottleLength, rimLength) =>
+  (bottleLength - rimLength) * 2 * (radius * radius)
+
+function sum(num) {
+  const memo = {}
+  function sumRecursive(target, max) {
+    let partitions = 0
+    for (let i = Math.min(target, max); i > 0; i--) {
+      if (i === target || i === 1) partitions++
+      else
+        partitions += memo[`${target - i}-${i}`] || sumRecursive(target - i, i)
+    }
+    memo[`${target}-${max}`] = partitions
+    return partitions
+  }
+  return sumRecursive(num, num)
+}
